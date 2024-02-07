397d5ef7bb26be9798bb07c84ebcc8576f3104f2
GovindSharma07 <govindsharma97037@gmail.com>
2024-02-07 12:09:43 +0530

    Correct route file

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,12 +1,12 @@
-const PushNotification = require("../controller/firebase.controller");
+const firebaseApi = require("../controller/firebase.controller");
  
 const express = require("express")
 const routes = express.Router();
 
 
-routes.post("/send-normal-notification", PushNotification.sendNormalPushNotification);
-routes.post("/sendToTopic",PushNotification.SendLocationToTopic);
-routes.post('/sendLocationToMultiUser',PushNotification.sendLocationToMultiUser);
+routes.post("/send-normal-notification", firebaseApi.sendNormalPushNotification);
+routes.post("/sendToTopic",firebaseApi.SendLocationToTopic);
+routes.post('/sendLocationToMultiUser',firebaseApi.sendLocationToMultiUser);
 
 routes.post("/createNewUser",firebaseApi.createNewUser);
 module.exports = routes;
