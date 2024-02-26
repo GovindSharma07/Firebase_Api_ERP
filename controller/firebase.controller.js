b5a1bf239601ffdcc825bb3fb97fc894a30f0a1b
GovindSharma07 <govindsharma97037@gmail.com>
2024-02-26 19:07:03 +0530

    added feature to add Driver Detail To database

--- controller/firebase.controller.js
+++ controller/firebase.controller.js
@@ -10,7 +10,9 @@ admin.initializeApp(
     }
 );
 
+const db = admin.firestore();
 
+//Sending Normal Notification to users using token
 exports.sendNormalPushNotification = async (req, res, next) => {
     try {
         const message = {
@@ -33,7 +35,7 @@ exports.sendNormalPushNotification = async (req, res, next) => {
     }
 }
 
-
+//Sending location with the help push notification
 exports.SendLocationToTopic = async (req, res, next) => {
     try {
         const message = {
@@ -57,6 +59,7 @@ exports.SendLocationToTopic = async (req, res, next) => {
     }
 }
 
+//sending location to multiple Users using push notification
 exports.sendLocationToMultiUser = async (req, res, next) => {
     try {
         const message = {
@@ -77,26 +80,60 @@ exports.sendLocationToMultiUser = async (req, res, next) => {
     }
 }
 
-
-exports.createUser = async (req,res,next)=>{
-    try{
+//creating user using email and password and sending its uid
+exports.createUser = async (req, res, next) => {
+    try {
         const email = req.body.email;
         const password = req.body.password;
         try {
-            const userRecord = await admin.auth().getUserByEmail(email);    
-            return res.send([true,userRecord]);
+            const userRecord = await admin.auth().getUserByEmail(email);
+            return res.send([true, userRecord]);
         } catch (error) {
-            if(error["code"] == "auth/user-not-found"){
+            if (error["code"] == "auth/user-not-found") {
                 await admin.auth().createUser({
-                    email : email,
-                    password : password
-                }).then((cred)=>{
-                    return res.send([true,cred]);
+                    email: email,
+                    password: password
+                }).then((cred) => {
+                    return res.send([true, cred]);
                 });
             }
         }
     }
-    catch(err){
-        return res.send([false,err]);
+    catch (err) {
+        return res.send([false, err]);
+    }
+}
+
+
+//adding driver details to the database
+exports.addDriverDetails = async (req, res, next) => {
+    try {
+        const uid = req.body.uid;
+        const email = req.body.email;
+        const fName = req.body.fName;
+        const lName = req.body.lName;
+        const busNumber = req.body.busNumber;
+        const contact = req.body.contact;
+        const address = req.body.address;
+
+        console.log(req.body);
+        const details = {
+            "Uid": uid,
+            "Email": email,
+            "First Name": fName,
+            "Last Name": lName,
+            "Bus Number": busNumber,
+            "Contact": contact,
+            "Address": address
+        }
+
+        const docRef = db.collection("User-Driver").doc(uid);
+
+        const response = await docRef.set(details);
+        return res.send(true);
+
+    }
+    catch (err) {
+        return res.send(false);
     }
 }
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,12 +1,13 @@
 const firebaseApi = require("../controller/firebase.controller");
- 
+
 const express = require("express")
 const routes = express.Router();
 
 
 routes.post("/send-normal-notification", firebaseApi.sendNormalPushNotification);
-routes.post("/sendToTopic",firebaseApi.SendLocationToTopic);
-routes.post('/sendLocationToMultiUser',firebaseApi.sendLocationToMultiUser);
+routes.post("/sendToTopic", firebaseApi.SendLocationToTopic);
+routes.post('/sendLocationToMultiUser', firebaseApi.sendLocationToMultiUser);
 
-routes.post("/createUser",firebaseApi.createUser);
+routes.post("/user/createUser", firebaseApi.createUser);
+routes.post("/user/addDriverDetails", firebaseApi.addDriverDetails);
 module.exports = routes;
