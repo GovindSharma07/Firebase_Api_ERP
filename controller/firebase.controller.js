718854f1c628e031fc72a3e6955633da5820f3ba
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-02-26 19:40:09 +0530

    Update firebase.controller.js

--- controller/firebase.controller.js
+++ controller/firebase.controller.js
@@ -130,10 +130,10 @@ exports.addDriverDetails = async (req, res, next) => {
         const docRef = db.collection("User-Driver").doc(uid);
 
         const response = await docRef.set(details);
-        return res.send([true,response.body]);
+        return res.send([true]);
 
     }
     catch (err) {
-        return res.send([false,err.body]);
+        return res.send([false]);
     }
 }
